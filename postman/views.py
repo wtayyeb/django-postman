dfb41521ee9fbc7e0b9da92effabed96e84985a4
Patrick Samson <pk.samson@gmail.com>
2013-10-27 10:51:46 +0100

    Renamed test_urls.py to urls_for_tests.py ; Converted some translations to lazy

--- postman/views.py
+++ postman/views.py
@@ -19,7 +19,7 @@ try:
 except ImportError:
     from datetime import datetime
     now = datetime.now
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext as _, ugettext_lazy as lz_
 from django.views.decorators.csrf import csrf_protect
 from django.views.generic import FormView, TemplateView, View
 
@@ -403,18 +403,18 @@ class UpdateMessageMixin(object):
 class ArchiveView(UpdateMessageMixin, View):
     """Mark messages/conversations as archived."""
     field_bit = 'archived'
-    success_msg = _("Messages or conversations successfully archived.")
+    success_msg = lz_("Messages or conversations successfully archived.")
     field_value = True
 
 
 class DeleteView(UpdateMessageMixin, View):
     """Mark messages/conversations as deleted."""
     field_bit = 'deleted_at'
-    success_msg = _("Messages or conversations successfully deleted.")
+    success_msg = lz_("Messages or conversations successfully deleted.")
     field_value = now()
 
 
 class UndeleteView(UpdateMessageMixin, View):
     """Revert messages/conversations from marked as deleted."""
     field_bit = 'deleted_at'
-    success_msg = _("Messages or conversations successfully recovered.")
+    success_msg = lz_("Messages or conversations successfully recovered.")
