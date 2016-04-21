9cea6944c93026b21959fcfaa3f2eaf8bb59f39a
Martin Pauly <m.pauly@jumart.de>
2016-04-21 14:27:09 +0200

    Added site to context of notification mail

--- postman/utils.py
+++ postman/utils.py
@@ -96,7 +96,7 @@ def notify_user(object, action, site):
         return
     if notification:
         # the context key 'message' is already used in django-notification/models.py/send_now() (v0.2.0)
-        notification.send(users=[user], label=label, extra_context={'pm_message': object, 'pm_action': action})
+        notification.send(users=[user], label=label, extra_context={'pm_message': object, 'pm_action': action, 'pm_site': site})
     else:
         if not DISABLE_USER_EMAILING and user.email and user.is_active:
             email('postman/email_user_subject.txt', 'postman/email_user.txt', [user.email], object, action, site)
